86f9d1498d73f5c347e12e175df6be166bde0367
Aleksey <arhangelruh@gmail.com>
2024-07-05 08:28:10 +0300

    init: change UI, add method for saving workplace code

---
+++ src/RatesMoney/Constants/PreferenceConstants.cs
@@ -0,0 +1,30 @@
+﻿namespace RatesMoney.Constants
+{
+	public static class PreferenceConstants
+	{
+		/// <summary>
+		/// Workplace code for currencies.
+		/// </summary>
+		public const string WorkplaceCode = nameof(WorkplaceCode);
+
+		/// <summary>
+		/// URL for connecting with API
+		/// </summary>
+		public const string APIService = nameof(APIService);
+
+		/// <summary>
+		/// Shared folder for Advertising.
+		/// </summary>
+		public const string AdvertisingFolder = nameof(AdvertisingFolder);
+
+		/// <summary>
+		/// Login for advertising folder.
+		/// </summary>
+		public const string AdvertisingLogin = nameof(AdvertisingLogin);
+
+		/// <summary>
+		/// Password for advertising folder.
+		/// </summary>
+		public const string AdvertisingPassword = nameof(AdvertisingPassword);
+	}
+}
---
+++ src/RatesMoney/Services/Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace RatesMoney.Services
+{
+	public class Configuration
+	{
+		public Task SaveWorkplaceCode(int workplace)
+		{
+			Preferences.Default.Set("Workplace_code", workplace);	
+			return Task.CompletedTask;
+		}
+	}
+}
