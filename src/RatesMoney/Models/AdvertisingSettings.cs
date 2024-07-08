8af28d4ae185417da319189ecc3167be09c998be
Ruh <ruh@tut.by>
2024-07-08 13:51:25 +0300

    init: add new logic for save API and Advertising settings.

---
+++ src/RatesMoney/Models/AdvertisingSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RatesMoney.Models
+{
+    public class AdvertisingSettings
+    {
+        /// <summary>
+        /// Path to folder with images.
+        /// </summary>
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+		[StringLength(1000,MinimumLength = 1, ErrorMessage ="Длинна")]
+		public string FolderPath { get; set; }
+
+        /// <summary>
+        /// User login.
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// User password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ src/RatesMoney/Services/AdvertisingConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using RatesMoney.Constants;
+using RatesMoney.Models;
+
+namespace RatesMoney.Services
+{
+	public class AdvertisingConfiguration
+	{
+		public void SaveAdvertisingSettings(AdvertisingSettings model)
+		{ 			
+			if (model != null)
+			{
+				if (model.FolderPath != null) {
+					Preferences.Default.Set(PreferenceConstants.AdvertisingFolder, model.FolderPath);
+				}
+				if (model.Login !=null ) {
+					Preferences.Default.Set(PreferenceConstants.AdvertisingLogin, model.Login);
+				}
+				if (model.Password != null)
+				{
+					Preferences.Default.Set(PreferenceConstants.AdvertisingPassword, model.Password);
+				}
+			}
+		}
+	}
+}
